feaf11eabd6ac9126d30c7ff4e55ad2cd57ed28f
kristjanjansen <kristjan.jansen@gmail.com>
2019-02-27 09:13:07 +0200

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,15 +5,3 @@
     </Links>
   </PageLayout>
 </template>
-
-<style>
-/*
-[
-        {img: '//media.voog.com/0000/0042/2942/photos/esimene.png', title: 'Lavastused', to: 'shows'},
-        {img: '//media.voog.com/0000/0042/2942/photos/esi_II.png', title: 'Kontserdid', to: '/music.html'},
-        {img: '//media.voog.com/0000/0042/2942/photos/esi_III.png',title: 'Näitused', to: '/showing.html'},
-        {img: '//media.voog.com/0000/0042/2942/photos/esi_IV.png', title: 'Inimesed', to: '/people.html'},
-        {img: '//media.voog.com/0000/0042/2942/photos/esimene.png', title: 'Sündmused', to: '/other.html'}
-      ]
-      */
-</style>
